a4c0c15a89d1a4f665dcdbf94c077da2aa8f4dc5
sanjeev-thapa <stedwardson@gmail.com>
2022-01-05 05:59:17 +0545

    Create Home Component

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>All Blogs</h2>
+            <div className="blog">
+                <h2>Blog Titile</h2>
+                <p>Written By: author</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
